c13b0c3624a950d690c018a1a239df3e58cfa930
apex13p <apex13p@gmail.com>
2022-03-30 21:42:26 -0400

    module 3, but tests fail

---
+++ src/app/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = "../assets/album.json";
+
+  constructor(private _http: Http) { }
+
+  /**
+    * getAlbum
+    */
+  getAlbum( id: number) {
+    return this._http
+      .get(this._albumUrl)
+      .map((response: Response) => response.json());
+    //return this._http.get(this._albumUrl).map(response => response.json());
+  }
+}
